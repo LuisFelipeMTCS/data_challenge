55420f01a3257c576d20c25eb480d36caa1ae26d
LuisFelipeMTCS <luisfelipetoledo.profissional@gmail.com>
2023-02-13 23:33:36 -0300

    CRIANDO ARQUIVO CONN MYSQL

--- db/mysql/conn.py
+++ db/mysql/conn.py
@@ -4,10 +4,12 @@ class Conn:
      
   def conecta_db(self):
     try:
-      con = mysql.connector.connect(host='localhost', 
-                            dbname='northwind',
-                            user='postgres', 
-                            password='123')
+      con = mysql.connector.connect(
+                            host='127.0.0.1', 
+                            port = '3306',
+                            database='northwind',
+                            user='root', 
+                            password='mysql123')
       return con
     except Exception as e:
           print(e)
